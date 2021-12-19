58cdc8ad25cfc80b7a5610463b11454b7e860d33
naufaladrna08 <naufaladrn90@gmail.com>
2021-12-20 01:50:23 +0700

    Add menu

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -19,6 +19,8 @@ MainWindow::MainWindow(QWidget* parent)
   QHBoxLayout* hlayout1 = new QHBoxLayout();
   QHBoxLayout* hlayout2 = new QHBoxLayout();
   QVBoxLayout* vlayout = new QVBoxLayout();
+
+  hlayout1->setContentsMargins(0, 26, 0, 0);
   hlayout1->addWidget(m_hrs);
   hlayout1->addWidget(m_randomLabel);
   hlayout1->addWidget(m_min);
@@ -26,10 +28,54 @@ MainWindow::MainWindow(QWidget* parent)
 
   vlayout->addLayout(hlayout1);
   vlayout->addLayout(hlayout2);
+  vlayout->setContentsMargins(8, 8, 8, 8);
 
   setLayout(vlayout);
+  createActions();
+  createMenus();
 }
 
 MainWindow::~MainWindow() {
+  delete m_hrs;
+  delete m_min;
+  delete m_randomLabel;
+  delete m_ringtones;
+}
 
+void MainWindow::createActions() {
+  m_exitAct = new QAction(tr("&Exit"), this);
+  m_exitAct->setShortcut(QKeySequence::Quit);
+  m_exitAct->setStatusTip(tr("Quit program"));
+
+  m_aboutAct = new QAction(tr("&About"), this);
+  m_aboutQtAct = new QAction(tr("&About QT"), this);
+
+  connect(m_exitAct, &QAction::triggered, this, &MainWindow::exit);
+  connect(m_aboutAct, &QAction::triggered, this, &MainWindow::about);
+  connect(m_aboutQtAct, &QAction::triggered, this, &MainWindow::aboutQt);
+}
+
+void MainWindow::createMenus() {
+  m_menu = new QMenuBar(this);
+  m_menu->setNativeMenuBar(false);
+  m_menu->setMinimumWidth(8000);
+
+  m_fileMenu = m_menu->addMenu(tr("&File"));
+  m_fileMenu->addAction(m_exitAct);
+
+  m_helpMenu = m_menu->addMenu(tr("&Help"));
+  m_helpMenu->addAction(m_aboutAct);
+  m_helpMenu->addAction(m_aboutQtAct);
+}
+
+void MainWindow::exit() {
+
+}
+
+void MainWindow::about() {
+
+}
+
+void MainWindow::aboutQt() {
+  
 }
--- MainWindow.hpp
+++ MainWindow.hpp
@@ -7,6 +7,9 @@
 class QLineEdit;
 class QLabel;
 class QComboBox;
+class QMenu;
+class QAction;
+class QMenuBar;
 
 class MainWindow : public QWidget {
   Q_OBJECT
@@ -15,11 +18,28 @@ class MainWindow : public QWidget {
     explicit MainWindow(QWidget* parent = 0);
     ~MainWindow();
 
+  protected slots:
+    void exit();
+    void about();
+    void aboutQt();
+
   private:
     QLineEdit* m_hrs;
     QLineEdit* m_min;
     QLabel* m_randomLabel;
     QComboBox* m_ringtones;
+
+    /* Context menu stuff */
+    void createActions();
+    void createMenus();
+
+    QMenuBar* m_menu;
+    QMenu* m_fileMenu;
+    QMenu* m_helpMenu;
+    QAction* m_exitAct;
+    QAction* m_aboutAct;
+    QAction* m_aboutQtAct;
+    QLabel* m_infoLabel;
 };
 
 #endif // __MAINWINDOW_H__
